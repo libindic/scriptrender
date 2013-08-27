17fd33fd4d811dbf3a8914ae3213fe60d1dcdeb1
Diadara <nithin111@gmail.com>
2013-08-27 20:55:59 +0530

    PEP8 cleaning

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
@@ -6,17 +5,17 @@ from setuptools import setup, find_packages
 name = "render"
 
 setup(
-    name = name,
-    version = "0.2.1",
-    url = "http://silpa.org.in/NGram",
-    license = "LGPL-3.0",
-    description = "Convert wiki pages to pdf and text into images",
-    author = "Santhosh Thottingal",
-    author_email = "santhosh.thottingal@gmail.com",
-    long_description = """Convert wiki pages to pdf and text into images""",
-    packages = find_packages(),
-    include_package_data = True,
-    setup_requires = ['setuptools-git'],
-    install_requires = ['setuptools','hyphenation', 'pypdflib', 'pyquery'],
-    zip_safe = False,
-    )
+    name=name,
+    version="0.2.1",
+    url="http://silpa.org.in/NGram",
+    license="LGPL-3.0",
+    description="Convert wiki pages to pdf and text into images",
+    author="Santhosh Thottingal",
+    author_email="santhosh.thottingal@gmail.com",
+    long_description="""Convert wiki pages to pdf and text into images""",
+    packages=find_packages(),
+    include_package_data=True,
+    setup_requires=['setuptools-git'],
+    install_requires=['setuptools', 'hyphenation', 'pypdflib', 'pyquery'],
+    zip_safe=False,
+)
