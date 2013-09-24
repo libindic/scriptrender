8d3e1296ac32e2d13d6b7b5ec93bb942fc6a84cb
Vasudev Kamath <kamathvasudev@gmail.com>
2013-09-24 21:52:14 +0530

    Version changed to 0.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ name = "scriptrender"
 
 setup(
     name=name,
-    version="0.2.1",
+    version="0.1",
     url="http://silpa.org.in/NGram",
     license="LGPL-3.0",
     description="Convert wiki pages to pdf and text into images",
